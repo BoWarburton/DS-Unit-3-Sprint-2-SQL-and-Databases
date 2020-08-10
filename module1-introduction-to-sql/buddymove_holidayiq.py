8c4f4007e5ace44e4ceb618611d5d8b77de7d78a
BoWarburton <bo@warburton.com>
2020-08-10 13:16:12 -0600

    Adds buddymove queries

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -8,37 +8,32 @@ print(f'Shape: {df.shape}')
 print(f'Missing values: \n {df.isnull().sum()}')
 print(f'df.describe: \n {df.describe()}')
 
-#Open a connection to a new (blank) database file buddymove_holidayiq.sqlite3
 conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
 
-#Use df.to_sql (documentation) to insert the data into a new table review in the SQLite3 database
 conn.execute("DROP TABLE IF EXISTS review")
 df.to_sql('review', con=conn)
-conn.execute("SELECT * FROM review").fetchall()
-
-#Then write the following queries (also with sqlite3) to test:
-
-#Count how many rows you have - it should be 249!
+# conn.execute("SELECT * FROM review").fetchall()
 cursor = conn.cursor()
 how_many_rows = "SELECT COUNT('User Id') FROM review"
-print(f'How many rows: {str(cursor.execute(how_many_rows).fetchall()[0][0])}')
+print(f'How many rows: '
+      f'{str(cursor.execute(how_many_rows).fetchall()[0][0])}')
 
-# How many users who reviewed at least 100 Nature in the category also reviewed at least 100 in the Shopping category?
 superuser_count = "SELECT count('User Id') \
-   FROM review \
-   WHERE Shopping > 100 \
-   AND Nature > 100"
+    FROM review \
+    WHERE Shopping > 100 \
+    AND Nature > 100"
 
-print(f'Superusers: {str(cursor.execute(superuser_count).fetchall()[0][0])}')
+print(f'Superusers: '
+      f'{str(cursor.execute(superuser_count).fetchall()[0][0])}')
 
-#(Stretch) What are the average number of reviews for each category?
-averages = "SELECT AVG(Shopping) AS 'Shopping', \
-    AVG(Picnic) AS 'Picnic', \
-    AVG(Sports) AS 'Sports', \
-    AVG(Religious) AS 'Religious', \
-    AVG(Nature) AS 'Nature', \
-    AVG(Theatre) AS 'Theatre' \
-    FROM review" 
-print(str(cursor.execute(averages).fetchall()))
-
-print(df.Shopping.mean())
+# #(Stretch) What are the average number of reviews for each category?
+# averages = "SELECT AVG(Shopping) AS 'Shopping', \
+#     AVG(Picnic) AS 'Picnic', \
+#     AVG(Sports) AS 'Sports', \
+#     AVG(Religious) AS 'Religious', \
+#     AVG(Nature) AS 'Nature', \
+#     AVG(Theatre) AS 'Theatre' \
+#     FROM review"
+# print(str(cursor.execute(averages).fetchall()))
+#
+# print(df.Shopping.mean())
--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -1,35 +1,61 @@
 #!/usr/bin/env Python
 import sqlite3
-import pandas as pd
-
-# 1st connect to db
 conn = sqlite3.connect('rpg_db.sqlite3')
-
-# 2nd make a cursor
 cursor = conn.cursor()
 
-# 3rd make SQL as Python string
 character_count = "SELECT count(*) FROM charactercreator_character"
+
 cleric_count = "SELECT count(*) FROM charactercreator_cleric"
+# cleric_count = "SELECT count(*) \
+#     FROM charactercreator_character, charactercreator_cleric \
+#     WHERE character_id = character_ptr_id"
+
 fighter_count = "SELECT count(*) FROM charactercreator_fighter"
+# fighter_count = "SELECT count(*) \
+#     FROM charactercreator_character, charactercreator_fighter \
+#     WHERE character_id = character_ptr_id"
+
 mage_count = "SELECT count(*) FROM charactercreator_mage"
+# mage_count = "SELECT count(*) \
+#     FROM charactercreator_character, charactercreator_mage \
+#     WHERE character_id = character_ptr_id"
+
 necromancer_count = "SELECT count(*) FROM charactercreator_necromancer"
+# necromancer_count = "SELECT count(*) \
+#     FROM charactercreator_character, charactercreator_necromancer \
+#     WHERE character_id = mage_ptr_id"
+
 thief_count = "SELECT count(*) FROM charactercreator_thief"
+# thief_count = "SELECT count(*) \
+#     FROM charactercreator_character, charactercreator_thief \
+#     WHERE character_id = character_ptr_id"
+
 item_count = "SELECT count(*) FROM armory_item"
+
 weapon_count = "SELECT count(*) FROM armory_weapon"
+
+not_weapon_count = "SELECT count(*) \
+    FROM armory_item a \
+    LEFT JOIN armory_weapon b \
+    ON a.item_id = b.item_ptr_id \
+    WHERE b.item_ptr_id IS NULL;"
+
 items_carried = "SELECT count(character_id) \
     FROM charactercreator_character_inventory \
     GROUP BY character_id LIMIT(20)"
+
 weapons_carried = "SELECT count(character_id) \
     FROM charactercreator_character_inventory, armory_weapon \
     WHERE item_id = item_ptr_id \
     GROUP BY character_id LIMIT(20)"
+
 items_average = "SELECT AVG(count) \
     FROM \
         (SELECT COUNT(character_id) \
         AS count \
         FROM charactercreator_character_inventory \
         GROUP BY character_id)" 
+
 weapons_average = "SELECT AVG(count) \
     FROM \
         (SELECT COUNT(character_id) \
@@ -37,40 +63,30 @@ weapons_average = "SELECT AVG(count) \
         FROM charactercreator_character_inventory, armory_weapon \
         WHERE item_id = item_ptr_id \
         GROUP BY character_id)"
-# cleric_count = "SELECT count(*) \
-#     FROM charactercreator_character, charactercreator_cleric \
-#     WHERE character_id = character_ptr_id"
 
-# fighter_count = "SELECT count(*) \
-#     FROM charactercreator_character, charactercreator_fighter \
-#     WHERE character_id = character_ptr_id"
-
-# mage_count = "SELECT count(*) \
-#     FROM charactercreator_character, charactercreator_mage \
-#     WHERE character_id = character_ptr_id"
-
-# necromancer_count = "SELECT count(*) \
-#     FROM charactercreator_character, charactercreator_necromancer \
-#     WHERE character_id = mage_ptr_id"
-
-# thief_count = "SELECT count(*) \
-#     FROM charactercreator_character, charactercreator_thief \
-#     WHERE character_id = character_ptr_id"
-
-# 4th cursor execute and fetchall
-print(f'How many total Characters: {str(cursor.execute(character_count).fetchall()[0][0])}')
-# print(f'Type of first element of first element of fetchall is {type(cursor.execute(character_count).fetchall()[0][0])}')
-# print(f'Type of fetchall is {type(str(cursor.execute(character_count).fetchall()[0][0])}')
-# print(f'Type of cursor.execute is {type(cursor.execute(character_count))}')
-print(f'How many of subclass cleric: {str(cursor.execute(cleric_count).fetchall()[0][0])}')
-print(f'How many of subclass fighter: {str(cursor.execute(fighter_count).fetchall()[0][0])}')
-print(f'How many of subclass mage: {str(cursor.execute(mage_count).fetchall()[0][0])}')
-print(f'How many of subclass necromancer: {str(cursor.execute(necromancer_count).fetchall()[0][0])}')
-print(f'How many of subclass thief: {str(cursor.execute(thief_count).fetchall()[0][0])}')
-print(f'How many total Items: {str(cursor.execute(item_count).fetchall()[0][0])}')
-print(f'How many of the Items are weapons: {str(cursor.execute(weapon_count).fetchall()[0][0])}')
-# print(f'How many are not? {str(cursor.execute(')
-print(f'How many items does each character have (first 20 rows): {cursor.execute(items_carried).fetchall()}')
-print(f'How many Weapons does each character have (first 20 rows): {cursor.execute(weapons_carried).fetchall()}')
-print(f'On average, how many Items does each Character have? {cursor.execute(items_average).fetchall()}')
-print(f'On average, how many Weapons does each Character have? {cursor.execute(weapons_average).fetchall()}')
+print(f'How many total Characters: '
+      f'{str(cursor.execute(character_count).fetchall()[0][0])}')
+print(f'How many of subclass cleric: '
+      f'{str(cursor.execute(cleric_count).fetchall()[0][0])}')
+print(f'How many of subclass fighter: '
+      f'{str(cursor.execute(fighter_count).fetchall()[0][0])}')
+print(f'How many of subclass mage: '
+      f'{str(cursor.execute(mage_count).fetchall()[0][0])}')
+print(f'How many of subclass necromancer: '
+      f'{str(cursor.execute(necromancer_count).fetchall()[0][0])}')
+print(f'How many of subclass thief: '
+      f'{str(cursor.execute(thief_count).fetchall()[0][0])}')
+print(f'How many total Items: '
+      f'{str(cursor.execute(item_count).fetchall()[0][0])}')
+print(f'How many of the Items are weapons: '
+      f'{str(cursor.execute(weapon_count).fetchall()[0][0])}')
+print(f'How many are not: '
+      f'{str(cursor.execute(not_weapon_count).fetchall()[0][0])}')
+print(f'How many items does each character have (first 20 rows): '
+      f'{cursor.execute(items_carried).fetchall()}')
+print(f'How many Weapons does each character have (first 20 rows): '
+      f'{cursor.execute(weapons_carried).fetchall()}')
+print(f'On average, how many Items does each Character have? '
+      f'{cursor.execute(items_average).fetchall()}')
+print(f'On average, how many Weapons does each Character have? '
+      f'{cursor.execute(weapons_average).fetchall()}')
